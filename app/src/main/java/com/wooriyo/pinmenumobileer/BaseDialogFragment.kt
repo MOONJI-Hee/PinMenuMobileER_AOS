93add530ec696312a14535fe47f726367972b599
Moon Ji-Hee <answlgml6@gmail.com>
2023-07-19 18:25:45 +0900

    2023-07-19 Ver 1.1.1

--- app/src/main/java/com/wooriyo/pinmenumobileer/BaseDialogFragment.kt
+++ app/src/main/java/com/wooriyo/pinmenumobileer/BaseDialogFragment.kt
@@ -18,12 +18,15 @@ open class BaseDialogFragment: DialogFragment() {
                 AppHelper.hideKeyboard(context, currentFocus, ev)
                 return super.dispatchTouchEvent(ev)
             }
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
