779f4f6be6bf00bdc5379ebd0d2414a00039a4c8
Moon Ji-Hee <answlgml6@gmail.com>
2023-12-06 17:43:53 +0900

    2023-12-06

--- app/src/main/java/com/wooriyo/pinmenumobileer/util/Api.kt
+++ app/src/main/java/com/wooriyo/pinmenumobileer/util/Api.kt
@@ -500,15 +500,4 @@ interface Api {
         @Query("query") query : String
     ): Call<KakaoResultDTO>
 
-    //Pos 연동 - 주문 관련 Api
-
-    // 1. KPN (First Pos)
-    @GET("/")
-    fun sendFirst(
-        @Query("serviceId") serviceId: String,      // 서비스 아이디 (주문 : Order, 그외 : Inquiry)
-        @Query("apiId") apiId: String,              // Api 아이디 (주문 : OrderDetail, 그외 : MasterSeq, OpenStatus 등..)
-        @Query("HD_ShopCd") HD_ShopCd: String,      // 매장의 본사코드 (FirstPOS ASP 등록 본사 코드 4자리) (매장코드 넣어도 상관없음)
-        @Query("shopCd") shopCd: String,            // 매장코드 (FirstPOS ASP 등록 매장 코드 4자리)
-        @Query("data") data: JSONObject             // 전송데이터
-    ): Call<KpnResultDTO>
 }
